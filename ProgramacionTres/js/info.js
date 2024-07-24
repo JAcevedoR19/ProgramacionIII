19c5e25ad9ce71cf971271be76f8be84e84bb481
JAcevedoR19 <joanaar19@gmail.com>
2024-07-23 23:57:42 -0300

    Se modificó la vista de los géneros en el archivo description.html, así como para que se vea mejor en desktop, se agregó que se pueda visualizar la duración de las películas selecionadas

--- ProgramacionTres/js/info.js
+++ ProgramacionTres/js/info.js
@@ -22,6 +22,7 @@ const appDescription = new Vue({
                     this.overview = data.overview;
                     this.poster_path = data.poster_path;
                     this.movie.averageRating = data.vote_average.toFixed(1);
+                    this.movie.runtime = data.runtime;
 
                     const genreIds = data.genre_ids;
                     if (genreIds) {
