d1bfdf9575f1dfd2c6b4b3ca3bc8c8a67fbcfeb0
JAcevedoR19 <joanaar19@gmail.com>
2024-07-25 17:56:05 -0300

    Se corrigió el buscador, ya funciona perfectamente. Se agregaron clases en el css para el buscador

--- ProgramacionTres/js/data.js
+++ ProgramacionTres/js/data.js
@@ -14,13 +14,14 @@ const app = new Vue({
         carouselImages: [],
         searchTerm: '',
         filteredMovies: [],
-        },
-        mounted() {
-            this.getMovies();
-        },
-        watch: {
-            searchTerm: 'filterMovies' // Llama a filterMovies cada vez que searchTerm cambie
-        },
+        showSearchResults: false,
+    },
+    mounted() {
+        this.getMovies();
+    },
+    watch: {
+        searchTerm: 'filterMovies' // Llama a filterMovies cada vez que searchTerm cambie
+    },
     methods: {
         async getMovies() {
             const endPoint = 'https://api.themoviedb.org/3';
@@ -28,29 +29,26 @@ const app = new Vue({
             const API_URL = `${endPoint}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
 
             try {
-            const response = await fetch(API_URL);
-            const data = await response.json();
+                const response = await fetch(API_URL);
+                const data = await response.json();
 
-            this.carouselImages = data.results.slice(0, 3).map(movie => ({
-                src: `https://image.tmdb.org/t/p/original/${movie.poster_path}`,
-                alt: movie.title,
-            }));
+                this.carouselImages = data.results.slice(0, 3).map(movie => ({
+                    src: `https://image.tmdb.org/t/p/original/${movie.poster_path}`,
+                    alt: movie.title,
+                }));
 
-            this.lista = data.results;
-            this.filteredMovies = this.lista;
+                this.lista = data.results;
+                this.filteredMovies = this.lista;
 
-            console.log(data);
-            
-            this.completed = true;
+                console.log(data);
+                
+                this.completed = true;
             } catch (error) {
-            console.log(error);
+                console.log(error);
             }
-
-            
         },
 
         filterMovies() {
-            console.log(this.searchTerm);
             if (this.searchTerm === '') {
                 this.filteredMovies = this.lista;
             } else {
@@ -63,6 +61,19 @@ const app = new Vue({
         openDescription(movie) {
             console.log(movie.id);
             window.location = `description.html?id=${movie.id}`;
-        }
+        },
+
+        hideSearchResults() {
+            setTimeout(() => {
+                this.showSearchResults = false;
+            }, 100); // Añadir un pequeño retraso para permitir que el evento 'mousedown' se dispare
+        },
+
+        selectMovie(movie) {
+            this.searchTerm = movie.title;
+            this.filteredMovies = [movie]; // Mostrar solo la película seleccionada
+            this.showSearchResults = false;
+            this.openDescription(movie); // Ir a la página de descripción de la película
+        },
     },
 });
