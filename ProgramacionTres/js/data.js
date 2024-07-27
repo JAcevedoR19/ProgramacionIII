9629651482e9ae2efcea59a606861e04740eb462
JAcevedoR19 <joanaar19@gmail.com>
2024-07-27 19:00:00 -0300

    Se actualizó el endpoint del buscador de películas para que puebuscar en toda la base de datos de la API y no sólo en el endpoint de las películas más populares

--- ProgramacionTres/js/data.js
+++ ProgramacionTres/js/data.js
@@ -27,19 +27,19 @@ const app = new Vue({
             const endPoint = 'https://api.themoviedb.org/3';
             const apiKey = 'eae71e9888ac9c5226c848e89f83ee1f';
             const API_URL = `${endPoint}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
-
+    
             try {
                 const response = await fetch(API_URL);
                 const data = await response.json();
-
+    
                 this.carouselImages = data.results.slice(0, 3).map(movie => ({
                     src: `https://image.tmdb.org/t/p/original/${movie.poster_path}`,
                     alt: movie.title,
                 }));
-
+    
                 this.lista = data.results;
                 this.filteredMovies = this.lista;
-
+    
                 console.log(data);
                 
                 this.completed = true;
@@ -47,33 +47,47 @@ const app = new Vue({
                 console.log(error);
             }
         },
-
+    
+        async searchMovies(query) {
+            const endPoint = 'https://api.themoviedb.org/3';
+            const apiKey = 'eae71e9888ac9c5226c848e89f83ee1f';
+            const API_URL = `${endPoint}/search/movie?query=${query}&api_key=${apiKey}`;
+    
+            try {
+                const response = await fetch(API_URL);
+                const data = await response.json();
+    
+                this.filteredMovies = data.results;
+                this.showSearchResults = true;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    
         filterMovies() {
             if (this.searchTerm === '') {
                 this.filteredMovies = this.lista;
             } else {
-                this.filteredMovies = this.lista.filter(movie => {
-                    return movie.title.toLowerCase().includes(this.searchTerm.toLowerCase());
-                });
+                this.searchMovies(this.searchTerm);
             }
         },
-
+    
         openDescription(movie) {
             console.log(movie.id);
             window.location = `description.html?id=${movie.id}`;
         },
-
+    
         hideSearchResults() {
             setTimeout(() => {
                 this.showSearchResults = false;
             }, 100); // Añadir un pequeño retraso para permitir que el evento 'mousedown' se dispare
         },
-
+    
         selectMovie(movie) {
             this.searchTerm = movie.title;
             this.filteredMovies = [movie]; // Mostrar solo la película seleccionada
             this.showSearchResults = false;
             this.openDescription(movie); // Ir a la página de descripción de la película
         },
-    },
+    },    
 });
